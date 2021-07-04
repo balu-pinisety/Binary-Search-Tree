1fd2b8555fa8a2149c61cac621c2028299e54f3b
balu-pinisety <manikantapandu225@gmail.com>
2021-07-04 21:19:03 +0530

    Added: 1) Calling commands in Main.java 2)Insert Method in BinarySearchTree.java 3) Consstructor, Getters and setters in Node.java

--- src/com/BinarySearchTree/BinarySearchTree.java
+++ src/com/BinarySearchTree/BinarySearchTree.java
@@ -7,4 +7,33 @@ package com.BinarySearchTree;
  */
 public class BinarySearchTree {
 	
+	//Creation of Node root
+	Node root;
+	
+	/**
+	 * Ability to insert Node in Binary Search Tree by calling method leftRight
+	 * @param data
+	 */
+	public void insert(int data) {
+		root = leftRight(root, data);
+	}
+	
+	/**
+	 * Ability to compare data with previous data
+	 * Selects position Left or Right by comparing
+	 * @param root, data
+	 * @return root
+	 */
+	public Node leftRight(Node root, int data) {
+		if (root == null) {
+			root = new Node(data);
+		}
+		if (data < root.getData()) {
+			root.setLeft(leftRight(root.left.getLeft(),data));
+		} else {
+			root.setRight(leftRight(root.getRight(),data));
+		}
+		return root;
+	}
+	
 }
